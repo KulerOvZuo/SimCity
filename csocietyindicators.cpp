1c3ec4bafaa1457f4943a8556fbe23855729231c
KulerOvZuo <andrzejkulesza6@gmail.com>
2014-05-20 19:12:25 +0200

    6.2

--- csocietyindicators.cpp
+++ csocietyindicators.cpp
@@ -3,14 +3,18 @@
 CSocietyIndicators::CSocietyIndicators()
 {
     birthIndicator=0;
+    children=0;
 }
 CSocietyIndicators::CSocietyIndicators(const CSocietyIndicators& _I)
 {
     peopleEarnings =_I.getPeopleEarnings();
     allPeople = _I.getAllPeople();
+    allLivingWorkingPeople = _I.getallLivingWorkingPeople();
+    allLivingNotWorkingPeople = _I.getallLivingNotWorkingPeople();
     allWorkForPeople =_I.getAllWorkForPeople();
     allLearningPeople =_I.getAllLearningPeople();
-    allProfessionsNeed =_I.getAllProfessionsNeed();
+    allProfessionsToEducate = _I.getAllProfessionsToEducate();
+    children = _I.getChildren();
     birthIndicator =_I.getBirthIndicator();
 }
 CSocietyIndicators::~CSocietyIndicators()
@@ -20,12 +24,18 @@ CPeopleEarnings CSocietyIndicators::getPeopleEarnings() const
 {   return peopleEarnings;}
 CPeople CSocietyIndicators::getAllPeople() const
 {   return allPeople;}
+CPeople CSocietyIndicators::getallLivingWorkingPeople() const
+{   return allLivingWorkingPeople;}
+CPeople CSocietyIndicators::getallLivingNotWorkingPeople() const
+{   return allLivingNotWorkingPeople;}
 CPeople CSocietyIndicators::getAllWorkForPeople() const
 {   return allWorkForPeople;}
 CPeople CSocietyIndicators::getAllLearningPeople() const
 {   return allLearningPeople;}
-CPeople CSocietyIndicators::getAllProfessionsNeed() const
-{   return allProfessionsNeed;}
+CPeople CSocietyIndicators::getAllProfessionsToEducate() const
+{   return allProfessionsToEducate;}
+int CSocietyIndicators::getChildren() const
+{   return children;}
 double CSocietyIndicators::getBirthIndicator() const
 {   return birthIndicator;}
 
@@ -33,11 +43,17 @@ void CSocietyIndicators::setPeopleEarnings(CPeopleEarnings _earn)
 {   peopleEarnings = _earn; }
 void CSocietyIndicators::setAllPeople(CPeople _people)
 {   allPeople = _people;}
+void CSocietyIndicators::setAllLivingWorkingPeople(CPeople _people)
+{   allLivingWorkingPeople = _people;}
+void CSocietyIndicators::setAllLivingNotWorkingPeople(CPeople _people)
+{   allLivingNotWorkingPeople = _people;}
 void CSocietyIndicators::setAllWorkForPeople(CPeople _people)
 {   allWorkForPeople = _people;}
 void CSocietyIndicators::setAllLearningPeople(CPeople _people)
 {   allLearningPeople = _people;}
-void CSocietyIndicators::setAllProfessionsNeed(CPeople _people)
-{   allProfessionsNeed = _people;}
+void CSocietyIndicators::setAllProfessionsToEducate(CPeople _people)
+{   allProfessionsToEducate = _people;}
+void CSocietyIndicators::setChildren(int _children)
+{   children = _children;}
 void CSocietyIndicators::setBirthIndicator(double _birth)
 {   birthIndicator = _birth;}
--- csocietyindicators.h
+++ csocietyindicators.h
@@ -24,22 +24,31 @@ public:
     void addPeopleEarnings(CPeopleEarnings _earn);
     void addAllPeople(CPeople _people);
     void addAllWorkForPeople(CPeople _people);
+    void addAllLivingWorkingPeople(CPeople _people);
+    void addAllLivingNotWorkingPeople(CPeople _people);
     void addAllLearningPeople(CPeople _people);
-    void addAllProfessionsNeed(CPeople _people);
+    void addAllProfessionsToEducate(CPeople _people);
+    void addChildren(int _children);
     void addBirthIndicator(double _birth);
 
     CPeopleEarnings getPeopleEarnings() const;
     CPeople getAllPeople() const;
-    CPeople getAllWorkForPeople() const;
+    CPeople getallLivingWorkingPeople() const;
+    CPeople getallLivingNotWorkingPeople() const;
     CPeople getAllLearningPeople() const;
-    CPeople getAllProfessionsNeed() const;
+    CPeople getAllWorkForPeople() const;
+    CPeople getAllProfessionsToEducate() const;
+    int getChildren() const;
     double getBirthIndicator() const;
 
     void setPeopleEarnings(CPeopleEarnings _earn);
     void setAllPeople(CPeople _people);
-    void setAllWorkForPeople(CPeople _people);
+    void setAllLivingWorkingPeople(CPeople _people);
+    void setAllLivingNotWorkingPeople(CPeople _people);
     void setAllLearningPeople(CPeople _people);
-    void setAllProfessionsNeed(CPeople _people);
+    void setAllWorkForPeople(CPeople _people);
+    void setAllProfessionsToEducate(CPeople _people);
+    void setChildren(int _children);
     void setBirthIndicator(double _birth);
 };
 
